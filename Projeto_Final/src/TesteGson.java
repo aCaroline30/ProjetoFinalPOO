12296d29caba1ddc194cf8215c87db3a2b605833
Ana Caroline <anacarolinefachini29@gmail.com>
2025-12-02 03:18:57 -0300

    Delete Projeto_Final directory

--- Projeto_Final/src/TesteGson.java
+++
@@ -1,8 +0,0 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-public class TesteGson {
-  public static void main(String[] args) {
-    Gson g = new GsonBuilder().create();
-    System.out.println("Gson ok: " + g);
-  }
-}
